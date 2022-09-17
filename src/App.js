2f87a93e61c8d1c9cddabe148de89b73a0a1fc1c
ivanniesala <Ivannie.c@gmail.com>
2022-09-17 13:17:12 -0400

    added google img search

--- src/App.js
+++ src/App.js
@@ -13,7 +13,45 @@ export default function App() {
           <Dictionary />
         </main>
         <footer className="App-footer">
-          <small>Coded by Ivannie</small>
+          <p id="resources">
+            Created by {""}
+            <a
+              href="https://www.linkedin.com/in/ivannie-saladin-736b50132/"
+              target="_blank"
+              rel="noreferrer"
+              className="link"
+            >
+              Ivannie Saladin {""}
+            </a>
+            student at {""}
+            <a
+              href="https://www.shecodes.io/"
+              target="_blank"
+              rel="noreferrer"
+              className="link"
+            >
+              Shecodes
+            </a>
+            <br />
+            Check out my repository on{" "}
+            <a
+              href="https://github.com/ivanniesala/Dictionary-Project"
+              target="_blank"
+              rel="noreferrer"
+              className="link"
+            >
+              GitHub {""}
+            </a>
+            hosted on{" "}
+            <a
+              href="https://app.netlify.com/teams/ivanniesala/overview"
+              target="_blank"
+              rel="noreferrer"
+              className="link"
+            >
+              Netlify
+            </a>
+          </p>
         </footer>
       </div>
     </div>
