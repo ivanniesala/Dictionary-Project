46f7b54b5d09d9ed1df49e10631cb02fead4a308
ivanniesala <Ivannie.c@gmail.com>
2022-09-17 12:16:43 -0400

    rendered the meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data[0]);
+    console.log(response.data[0].meanings[0].definitions[0].definition);
+
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -36,6 +41,8 @@ export default function Dictionary() {
           Search
         </button>
       </form>
+     
+      <Results results={results} />
     </div>
   );
 }
